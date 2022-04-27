5b0e749ad5eec6d8932d67ea42e4780ac1d890b4
Alex Chen <8888.alex@gmail.com>
2022-04-27 16:45:15 +0800

    .

---
+++ _setService.bat
@@ -0,0 +1,13 @@
+@echo off
+if \"%~1\"==\"\" exit 
+set input=%~n1Service
+set /p input="Input Service Name[%input%]:"
+set svcn="%~dp1%input%.exe"
+xcopy "%~dp0_unsetService.bat" "%~dp1" /q /y
+xcopy "%~dp0instsrv.exe" "%~dp1" /q /y
+xcopy "%~dp0srvany.exe" "%~dp1" /q /y
+ren "%~dp1srvany.exe" %input%.exe
+"%~dp0instsrv" %input% %svcn%
+reg add HKLM\SYSTEM\CurrentControlSet\services\%input%\Parameters /v Application /d "%~1"
+net start %input%
+pause
---
+++ _unsetService.bat
@@ -0,0 +1,9 @@
+@echo off
+if \"%~1\"==\"\" exit
+set input=%~n1
+net stop %input%
+"%~dp0\instsrv" %input% remove
+del "%~dp0\%~nx1" /Q
+del "%~dp0\instsrv.exe" /Q
+del "%~dp0\_unsetService.bat" /Q
+pause
