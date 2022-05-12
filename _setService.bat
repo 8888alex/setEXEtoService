ff34db508ae0503a51c4f6173f77cf7a4c2819b3
Alex Chen <8888.alex@gmail.com>
2022-05-12 08:22:15 +0800

    .

--- _setService.bat
+++ _setService.bat
@@ -3,7 +3,7 @@ if \"%~1\"==\"\" exit
 set input=%~n1Service
 set /p input="Input Service Name[%input%]:"
 set svcn="%~dp1%input%.exe"
-xcopy "%~dp0_unsetService.bat" "%~dp1" /q /y
+xcopy "%~dp0_unsetService.bat" "%~dp1%input%_unsetService.bat" /q /y
 xcopy "%~dp0instsrv.exe" "%~dp1" /q /y
 xcopy "%~dp0srvany.exe" "%~dp1" /q /y
 ren "%~dp1srvany.exe" %input%.exe
--- _unsetService.bat
+++ _unsetService.bat
@@ -3,8 +3,8 @@ if \"%~1\"==\"\" exit
 set input=%~n1
 net stop %input%
 "%~dp0\instsrv" %input% remove
+reg delete HKLM\SYSTEM\CurrentControlSet\services\%input% /f
 del "%~dp0\%~nx1" /Q
 del "%~dp0\instsrv.exe" /Q
-del "%~dp0\_unsetService.bat" /Q
-reg delete HKLM\SYSTEM\CurrentControlSet\services\%input% /f
+del "%~dp0\%input%_unsetService.bat" /Q
 pause
